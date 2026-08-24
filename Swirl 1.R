681670ab9ab3404ddab2a8e70989f0375f606069
Austin Patrick Wareham <austinwareham03@gmail.com>
2026-08-24 12:49:58 -0400

    Add files via upload

---
+++ Swirl 1.R
@@ -0,0 +1,40 @@
+# Swirl Lesson 1 
+library(swirl)
+install_course_github('isaaccloh', '377_swirl')
+#starting up swirl 
+swirl()
+Wareham
+3
+1
+1
+1
+1
+#type 5+7
+5+7
+
+#assigning a variable
+x <-  5+7
+#reviewing contents
+x
+y <- x-3
+y
+#creating Vector
+c(1.1,9,3.14)
+z <- c(1.1,9,3.14)
+?c
+z
+c(z,555,z)
+z*2 +1000
+my_sqrt <- sqrt(z-1)
+1
+my_sqrt
+my_div <- z/my_sqrt
+3
+my_div
+c(1,2,3,4)+c(0,10)
+c(1,2,3,4)
+#adding vectors that are uneven
+c(1,2,3,4)+c(0,10,100)
+#using up arrow to find command
+my_div
+1
